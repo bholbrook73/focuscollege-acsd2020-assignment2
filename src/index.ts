11fca0d795eb9648ee5a50ea6bf376bbd53fb43f
root <root@LAPTOP-GJRTG0G3.localdomain>
2020-09-12 11:44:48 -0700

    Created Own Index and saved Original in OriginalImport.ts

--- src/index.ts
+++ src/index.ts
@@ -1,58 +1,37 @@
-// import yargs to re-use code from someone else that has already 
-// solved the complexities of parsing command line arguments
+
+// importing and requiring yargs to use in apllication
 import yargs = require('yargs');
 
-let parameters = 
+let parameters
 
-// create a new yargs "command"
-yargs.command(
+// Creates a new yargs command:
+// FORMAT:  node dist/index.js [command] --[parameter] [value] --[parameter] [value]
+// EXAMPLE: node dist/index.js calc-wall-studs --width 8 --legth 8
 
-    // yargs.command paramater #1
-    // name the command with no spaces
-    // this will be the first parameter we send:
-    // format:  node dist/index.js [command]
-    // example: node dist/index.js calc-wall-studs
-    'calc-wood-needed',
+    yargs.command(
+    //Parameter #1 this will be what we type into the command line first
+    'calc-lumber-needed',
+    
+    //Parameter #2 describes the command so that the --help flag is helpful
+    'Calculate the number of 2x4s and 4x4s needed to build a house',
 
-    // yargs.command paramater #2
-    // describe the command so that the --help flag is helpful
-    'Calculate the number of studs required to stick frame a house for Gerald',
-
-    // yargs.command paramater #3
-    // define the parameters we need for our command
+    //Parameter #3 defines the parameters we need for our command
     {
-
-        // format:  node dist/index.js [command] --[parameter] [value]
-        // example: node dist/index.js calc-wall-studs --width 8
         width: {
-            
-            // format:  node dist/index.js [command] --[parameter] [value must be number]
-            // example: node dist/index.js calc-wall-studs --width 8
             type: 'number',
-
-            // format:  node dist/index.js [command] -[parameter] [value]
-            // example: node dist/index.js calc-wall-studs -w 8
-            alias: 'w',
-
-            // this adds the description for the --help flag
-            description: 'The width of the house'
-
+            // The alias can be used with a single '-' infront of the parameter
+            alias:'w',
+            description:"The width of the house"
         },
-
-        // format:  node dist/index.js [command] --[parameter] [value] --[parameter] [value]
-        // example: node dist/index.js calc-wall-studs --width 8 --legth 8
         length: {
             type: 'number',
+            // The alias can be used with a single '-' infront of the parameter
             alias: 'l',
             description: 'The length of the house'
         }
-
     },
 
-    // yargs.command parameter #4
-    // define the function we want to run once the arguments are parsed
-    // the parsed arguments will be provided via the first paramater passed in
-    // we've called it 'args' in this function
+    // Parameter 4 defines the function we want to run once the arguments are parsed
     function( args ){
         
         // ----------------------------------------------------------------------------------- |
@@ -60,15 +39,12 @@ yargs.command(
         // This is where you write your application with access to the command line arguments  |
         // ----------------------------------------------------------------------------------- |
 
-        // inspect the output to infer how you might access the arguments
-        console.log( args )
-
+        console.log(args);
     }
-
 );
 
-// tell yargs to include the --help flag
+// tell yargs to include the --help flag 
 yargs.help();
 
-// tell yargs to parse the parameters
+// tell yargs to parge the parameters
 yargs.parse();
