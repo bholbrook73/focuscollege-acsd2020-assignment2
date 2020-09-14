0222da9f3baf592f35b3ac9f79e3b3b8faa18656
Bradley Holbrook <bholbrook73@gmail.com>
2020-09-14 11:40:39 -0700

    Corrected Calculations

--- src/gerald/index.ts
+++ src/gerald/index.ts
@@ -1,10 +1,213 @@
-export function calculateHouseRequirements ( widthInFeet:number, lengthInFeet: number ){
+const BEAM_WIDTH = 3.5;
+const BOARD_LENGTH = ( 8 * 12 );
+const WASTE_MULTIPLIER = 0.1;
 
-    const requirements = {
-        boards: 0,
-        beams: 0
+// beams are required every 20 feet at minimum
+const BEAMS_REQUIRED_EVERY_INCHES = (20 * 12);
+const FULL_BOARDS_IN_SECTION = Math.floor( BEAMS_REQUIRED_EVERY_INCHES / BOARD_LENGTH )
+const FULL_BOARD_SECTION_SIZE = FULL_BOARDS_IN_SECTION * BOARD_LENGTH;
+
+console.log({
+    name: "contants",
+    BEAM_WIDTH,
+    BOARD_LENGTH,
+    WASTE_MULTIPLIER,
+    BEAMS_REQUIRED_EVERY_INCHES,
+    FULL_BOARD_SECTION_SIZE
+});
+
+function convertFeetToInches( feet:number ){
+
+    return feet * 12;
+
+}
+
+function getStudsInLength( inches:number ): number {
+
+    // devide the length by 96 inches (8 feet) and round up
+    // multiply by two because we're doing the top and bottom in one calculation
+    const topAndBottom = Math.ceil( inches / 96 ) * 2;
+
+    // calculate the studs across
+    // round up to account for the last one
+    let studs = Math.ceil( inches / 16 );
+
+    // make sure we add an end piece if we have a perfect multiple of 16
+    const isNotPerfectWidth = Math.min( inches % 16, 1 );
+    const perfectWidthExtension = (isNotPerfectWidth * -1) + 1;
+    studs += perfectWidthExtension;
+
+    console.log({
+        function: "getStudsInLength",
+        inches,
+        isNotPerfectWidth,
+        perfectWidthExtension,
+        topAndBottom,
+        studs
+    });
+
+    return topAndBottom + studs;
+
+}
+
+function getRequiredBeamsInLength( inches:number ){
+
+    // for every 20 feet, we need one beam
+    // we know our wall is at least 20 feet, so calculate the required beams for the REST of the wall
+    // if our wall is under 20 feet, this will return zero
+    const wallLengthOverMinRequired = getWallLengthOverMinimumRequiredBeforeBeam( inches );
+    const wallLengthPlusBeam = BEAMS_REQUIRED_EVERY_INCHES + BEAM_WIDTH;
+    const requiredBeams = Math.ceil( wallLengthOverMinRequired / wallLengthPlusBeam );
+
+    return requiredBeams;
+
+}
+
+function getWallLengthOverMinimumRequiredBeforeBeam( inches:number ): number {
+    return Math.max(inches - BEAMS_REQUIRED_EVERY_INCHES, 0);
+}
+
+// any number of inches past BEAMS_REQUIRED_EVERY_INCHES will return 1
+// any number of inches below or equal to BEAMS_REQUIRED_EVERY_INCHES return 0
+function isBeamRequired( inches:number ): number {
+    
+    // negative numbers are zero
+    const wallLengthOverMinRequired = Math.max(inches - BEAMS_REQUIRED_EVERY_INCHES, 0);
+
+    // remove decimals
+    const wholeNumber = Math.ceil( wallLengthOverMinRequired );
+
+    // returns 1 (at least one beam required ) or 0 (no beams required)
+    const isBeamRequired = Math.min( wholeNumber, 1 );
+
+    console.log({
+        function: "isBeamRequired",
+        inches,
+        wallLengthOverMinRequired,
+        wholeNumber,
+        isBeamRequired
+    });
+    
+    return isBeamRequired;
+
+}
+
+function getFullSections( inches:number, beams:number ){
+
+    // how many inches will we remove from a section between beams to get to the last full board
+    const inchesReducedPerSection = BEAMS_REQUIRED_EVERY_INCHES - FULL_BOARD_SECTION_SIZE;
+
+    // how big is the last section if all beams are at BEAMS_REQUIRED_EVERY_INCHES
+    const lastSectionSize = inches - ( beams * ( BEAMS_REQUIRED_EVERY_INCHES + BEAM_WIDTH ));
+
+    // how many inches of boards can we add to the last section before it will add an additional beam to the structure
+    const remainingBeforeNewBeam = BEAMS_REQUIRED_EVERY_INCHES - lastSectionSize;
+
+    // how many complete portions of the inchesReducedPerSection can we move to the last section
+    let fullSections = Math.floor( remainingBeforeNewBeam / inchesReducedPerSection );
+
+    // even if we can FIT fullSections moved into the last portion, we might not HAVE them in our length
+    fullSections = Math.min( fullSections, beams );
+
+    // safeguard inches not requiring a beam and return value
+    fullSections *= isBeamRequired( inches );
+
+    console.log({
+        function: "getFullSections",
+        inches,
+        beams,
+        inchesReducedPerSection,
+        lastSectionSize,
+        remainingBeforeNewBeam,
+        fullSections
+    });
+    
+    return fullSections;
+
+}
+
+function getLastSectionSize( inches:number, beams:number ){
+
+    const fullSections = getFullSections( inches, beams );
+    const lastSectionSize = inches - ( beams * BEAM_WIDTH ) - ( fullSections * FULL_BOARD_SECTION_SIZE );
+
+    console.log({
+        function: "getLastSectionSize",
+        inches,
+        beams,
+        fullSections,
+        lastSectionSize
+    });
+
+    return lastSectionSize;
+
+}
+
+function buildWall( inches:number ){
+
+    // get required beams
+    const requiredBeams = getRequiredBeamsInLength( inches );
+    const fullSections = getFullSections( inches, requiredBeams );
+    const lastSectionSize =  getLastSectionSize( inches, requiredBeams );
+    const studs = getStudsInLength( FULL_BOARD_SECTION_SIZE ) * fullSections + getStudsInLength( lastSectionSize );
+
+    const wall = {
+        function: "buildWall",
+        inches,
+        studs: studs,
+        beams: requiredBeams
     };
 
-    return requirements;
+    console.log( wall );
+
+    return wall;
+
+}
+
+function accountForWaste( items:number ):number {
+
+    const waste = Math.ceil( items * WASTE_MULTIPLIER );
+
+    console.log({
+        function: "accountForWaste",
+        items,
+        waste
+    });
+    
+    return waste + items;
+
+}
+
+export function calculateHouseRequirements( widthInFeet:number, lengthInFeet:number ){
+
+    // convert feet to inches
+    const outerWidthOfHouse = convertFeetToInches( widthInFeet );
+    const outerLengthOfHouse = convertFeetToInches( lengthInFeet );
+    
+    // calculate the space inbetween corner beams
+    const innerWidthOfHouse = outerWidthOfHouse - ( BEAM_WIDTH * 2 );
+    const innerLengthOfHouse = outerLengthOfHouse - ( BEAM_WIDTH * 2 );
+
+    const wall1 = buildWall( innerWidthOfHouse );
+    const wall2 = buildWall( innerLengthOfHouse );
+
+    const studs = accountForWaste(( wall1.studs + wall2.studs ) * 2);
+    const beams = accountForWaste((( wall1.beams + wall2.beams ) * 2) + 4);
+
+    return {
+        function: "calculateHouseRequirements",
+        width: {
+            widthInFeet,
+            outerWidthOfHouse,
+            innerWidthOfHouse
+        },
+        length: {
+            lengthInFeet,
+            outerLengthOfHouse,
+            innerLengthOfHouse
+        },
+        studs: studs,
+        beams: beams
+    };
 
 }
