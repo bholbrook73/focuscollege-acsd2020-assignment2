dc882081a9e47560d016eb8472fb3f9014ce34ba
root <root@LAPTOP-GJRTG0G3.localdomain>
2020-09-14 20:26:21 -0700

    Account for waste added to postsInBuilding

--- src/modules/postsInBuilding.ts
+++ src/modules/postsInBuilding.ts
@@ -1,4 +1,5 @@
 import calcInnerWallWidth from './calcInnerWallWidth';
+import accountForWaste from './accountForWaste';
 //This function will find out how many posts in the building
 export default function postsInBuilding(houseWidthInFeet:number, houseLengthInFeet:number){
 
@@ -11,13 +12,17 @@ export default function postsInBuilding(houseWidthInFeet:number, houseLengthInFe
 //IF the width is greater than 20' then execute the code inside
     if(houseWidthInFeet >= 20)
     {
-        postsInWallWidth = Math.ceil((houseWidthInFeet / 20) * 2);
+        postsInWallWidth = Math.floor((houseWidthInFeet / 20) * 2);
     } 
 //IF the length is greater than 20' then execute the code inside
     if(houseLengthInFeet >= 20){
-        postsInWallLength = Math.ceil((houseLengthInFeet / 20) * 2);
+        postsInWallLength = Math.floor((houseLengthInFeet / 20) * 2);
     }
 
+//totalPosts BEFORE waste is accounted for
     totalPosts += postsInWallWidth + postsInWallLength;
+
+//Accouts for the waste here
+totalPosts = accountForWaste(totalPosts);
     return totalPosts;
 }
