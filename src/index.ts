4e2206f9da05433d28c430d03dddef432634edac
Tyler Matzelle <tmatzelle16560@alumni.focuscollege.com>
2020-09-15 09:39:09 -0700

    integrated code to recive parameters and give calculated results

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 // import yargs to re-use code from someone else that has already 
 // solved the complexities of parsing command line arguments
 import yargs = require('yargs');
+import {calcWallStuds} from './gerald'
 
 let parameters = 
 
@@ -20,32 +21,34 @@ yargs.command(
 
     // yargs.command paramater #3
     // define the parameters we need for our command
-    {
+    function(yargs){
+        return yargs.options({
 
-        // format:  node dist/index.js [command] --[parameter] [value]
-        // example: node dist/index.js calc-wall-studs --width 8
-        width: {
-            
-            // format:  node dist/index.js [command] --[parameter] [value must be number]
+            // format:  node dist/index.js [command] --[parameter] [value]
             // example: node dist/index.js calc-wall-studs --width 8
-            type: 'number',
+            width: {
+            
+                // format:  node dist/index.js [command] --[parameter] [value must be number]
+                // example: node dist/index.js calc-wall-studs --width 8
+                type: 'number',
 
-            // format:  node dist/index.js [command] -[parameter] [value]
-            // example: node dist/index.js calc-wall-studs -w 8
-            alias: 'w',
+                // format:  node dist/index.js [command] -[parameter] [value]
+                // example:      -w 8
+                alias: 'w',
 
-            // this adds the description for the --help flag
-            description: 'The width of the house'
+                // this adds the description for the --help flag
+                description: 'The width of the house'
 
-        },
+            },
 
         // format:  node dist/index.js [command] --[parameter] [value] --[parameter] [value]
         // example: node dist/index.js calc-wall-studs --width 8 --legth 8
-        length: {
-            type: 'number',
-            alias: 'l',
-            description: 'The length of the house'
-        }
+            length: {
+                type: 'number',
+                alias: 'l',
+                description: 'The length of the house'
+            }
+        });
 
     },
 
@@ -55,13 +58,10 @@ yargs.command(
     // we've called it 'args' in this function
     function( args ){
         
-        // ----------------------------------------------------------------------------------- |
-        // NOTE TO STUDENTS:                                                                   |
-        // This is where you write your application with access to the command line arguments  |
-        // ----------------------------------------------------------------------------------- |
+        let geraldsWoodPurchase = calcWallStuds(Number(args.width), Number(args.length));
 
         // inspect the output to infer how you might access the arguments
-        console.log( args )
+        console.log(geraldsWoodPurchase);
 
     }
 
